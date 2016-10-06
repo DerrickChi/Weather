e20d60e1e4502f47d0515b202f25445a3d5a5bc7
Derrick Chang <iderrickchang@gmail.com>
2016-10-06 16:26:41 -0700

    c2fJNI returns double []

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -2,12 +2,32 @@
 #include <string>
 
 extern "C"
-jfloat
-Java_edu_ucla_derrickchang_weather_MainActivity_cfConvertJNI(
+jdoubleArray
+Java_edu_ucla_derrickchang_weather_MainActivity_c2fJNI(
         JNIEnv *env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    jfloat result = 1.0;
-    return result;
-    //return env->NewStringUTF(hello.c_str());
+        jobject /* this */,
+        jdoubleArray inJNIArray) {
+
+    // Step 1: Convert the incoming JNI jdoublearray to C's jdouble[]
+    jdouble *inCArray = env->GetDoubleArrayElements(inJNIArray, NULL);
+    if (NULL == inCArray) return NULL;
+    jsize length = env->GetArrayLength(inJNIArray);
+
+    // Step 2: Perform its intended operations
+    jint sum = 0;
+    int i;
+    for (i = 0; i < length; i++) {
+        sum += inCArray[i];
+    }
+    jdouble average = (jdouble)sum / length;
+    env->ReleaseDoubleArrayElements(inJNIArray, inCArray, 0); // release resources
+
+    jdouble outCArray[] = {sum, average};
+
+
+    // Step 3: Convert the C's Native jdouble[] to JNI jdoublearray, and return
+    jdoubleArray outJNIArray = env->NewDoubleArray(2);  // allocate
+    if (NULL == outJNIArray) return NULL;
+    env->SetDoubleArrayRegion(outJNIArray, 0 , 2, outCArray);  // copy
+    return outJNIArray;
 }
--- app/src/main/java/edu/ucla/derrickchang/weather/MainActivity.java
+++ app/src/main/java/edu/ucla/derrickchang/weather/MainActivity.java
@@ -29,9 +29,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
 
+        // Generate testing numbers
+        double[] numbers = {22.0, 33.0, 33.0};
+
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(Float.toString(cfConvertJNI()));
+        tv.setText(Double.toString(c2fJNI(numbers)[0]));
 
         // Get an instance of the sensor service, and use that to get an instance of
         // a particular sensor.
@@ -98,5 +101,5 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    public native float cfConvertJNI();
+    public native double[] c2fJNI(double[] numbers);
 }
