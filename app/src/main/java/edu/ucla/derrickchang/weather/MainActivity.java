cf0c9dc961f206822efcddac56d823c57d520948
Derrick Chang <iderrickchang@gmail.com>
2016-10-06 17:57:48 -0700

    add Mon-Fri, c-f toggle

--- app/src/main/java/edu/ucla/derrickchang/weather/MainActivity.java
+++ app/src/main/java/edu/ucla/derrickchang/weather/MainActivity.java
@@ -17,6 +17,8 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity implements SensorEventListener{
     private SensorManager mSensorManager;
     private Sensor mPressure;
+    private boolean isC = true;
+    private double[] numbers = new double[6];
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -32,15 +34,27 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         setSupportActionBar(myToolbar);
 
         // Generate testing numbers
-        double[] numbers = new double[6];
+
         Random rand = new Random();
         for (int i=0; i<numbers.length; i++) {
             numbers[i]=10.0 + (40.0 - 10.0) * rand.nextDouble();
         }
 
         // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(Double.toString(c2fJNI(numbers)[0]));
+        TextView tv1 = (TextView) findViewById(R.id.textViewMon);
+        tv1.setText("Mon: " + Double.toString(c2fJNI(numbers)[1]));
+
+        TextView tv2 = (TextView) findViewById(R.id.textViewTue);
+        tv2.setText("Tue: " + Double.toString(c2fJNI(numbers)[2]));
+
+        TextView tv3 = (TextView) findViewById(R.id.textViewWed);
+        tv3.setText("Wed: " + Double.toString(c2fJNI(numbers)[3]));
+
+        TextView tv4 = (TextView) findViewById(R.id.textViewThu);
+        tv4.setText("Thu: " + Double.toString(c2fJNI(numbers)[4]));
+
+        TextView tv5 = (TextView) findViewById(R.id.textViewFri);
+        tv5.setText("Fri: " + Double.toString(c2fJNI(numbers)[5]));
 
         // Get an instance of the sensor service, and use that to get an instance of
         // a particular sensor.
@@ -58,8 +72,16 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     public final void onSensorChanged(SensorEvent event) {
         float millibars_of_pressure = event.values[0];
         // Do something with this sensor data.
+        numbers[0] = millibars_of_pressure;
         TextView tv = (TextView) findViewById(R.id.ambient_temp);
-        tv.setText("Current pressuer: " + Float.toString(millibars_of_pressure));
+
+        if (isC) {
+            tv.setText("Ambient temperature: " + Double.toString(numbers[0]));
+        }
+        else {
+            tv.setText("Ambient temperature: " + Double.toString(c2fJNI(numbers)[0]));
+        }
+
     }
 
     @Override
@@ -93,6 +115,35 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             case R.id.action_convert:
                 // User chose the "Favorite" action, mark the current item
                 // as a favorite...
+                isC = !isC;
+
+                TextView tv = (TextView) findViewById(R.id.ambient_temp);
+                TextView tv1 = (TextView) findViewById(R.id.textViewMon);
+                TextView tv2 = (TextView) findViewById(R.id.textViewTue);
+                TextView tv3 = (TextView) findViewById(R.id.textViewWed);
+                TextView tv4 = (TextView) findViewById(R.id.textViewThu);
+                TextView tv5 = (TextView) findViewById(R.id.textViewFri);
+
+
+                if (isC) {
+                    tv.setText(": " + Double.toString(numbers[0]));
+                    tv1.setText("Mon: " + Double.toString(numbers[1]));
+                    tv2.setText("Tue: " + Double.toString(numbers[2]));
+                    tv3.setText("Wed: " + Double.toString(numbers[3]));
+                    tv4.setText("Thu: " + Double.toString(numbers[4]));
+                    tv5.setText("Fri: " + Double.toString(numbers[5]));
+
+                }
+                else
+                {
+                    tv.setText(": " + Double.toString(c2fJNI(numbers)[0]));
+                    tv1.setText("Mon: " + Double.toString(c2fJNI(numbers)[1]));
+                    tv2.setText("Tue: " + Double.toString(c2fJNI(numbers)[2]));
+                    tv3.setText("Wed: " + Double.toString(c2fJNI(numbers)[3]));
+                    tv4.setText("Thu: " + Double.toString(c2fJNI(numbers)[4]));
+                    tv5.setText("Fri: " + Double.toString(c2fJNI(numbers)[5]));
+                }
+
                 return true;
 
             default:
